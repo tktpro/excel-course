e761de03b5d1caab8d1a6e4a32e73c48b128eb21
Denis <miloru@gmail.com>
2022-01-26 18:03:48 +0200

    Implement "dependency inversion principle"

--- src/core/routes/Router.js
+++ src/core/routes/Router.js
@@ -1,3 +1,4 @@
+import Loader from '../../components/loader/Loader';
 import $ from '../dom';
 import ActiveRoute from './ActiveRoute';
 
@@ -9,6 +10,8 @@ export default class Router {
 
     this.$placeholder = $(selector);
     this.routes = routes;
+
+    this.loader = new Loader();
 
     this.page = null;
 
@@ -22,18 +25,20 @@ export default class Router {
     this.changePageHandler();
   }
 
-  changePageHandler() {
+  async changePageHandler() {
     if (this.page) {
       this.page.destroy();
     }
-    this.$placeholder.clear();
+    this.$placeholder.clear().append(this.loader);
 
     const Page = ActiveRoute.path.includes('excel')
       ? this.routes.excel
       : this.routes.dashboard;
     this.page = new Page(ActiveRoute.param);
 
-    this.$placeholder.append(this.page.getRoot());
+    const $root = await this.page.getRoot();
+
+    this.$placeholder.clear().append($root);
 
     this.page.afterRender();
   }
